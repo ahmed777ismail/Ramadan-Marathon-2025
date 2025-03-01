f98880c67b7427403f63346b6d8ae15c3ddca265
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2025-03-02 01:02:38 +0200

    Add files via upload
    A. Ramadan Dates

---
+++ A. Ramadan Dates/A. Ramadan Dates/A. Ramadan Dates.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+    if (X % 2 == 0) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << "Ramadan Kareem!" << endl;
+    }
+    return 0;
+}
